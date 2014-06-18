f4127260dfe60f0cd4cb8e37455851add10867ff
Harshavardhana <harsha@harshavardhana.net>
2014-06-17 23:05:44 -0700

    Minify js, add favicon.ico

--- make.js
+++ make.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Hyperbotics.org - Additional modifications
+ * Copyright 2014 Hyperbotics.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
